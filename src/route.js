cda236b954bfa562fb9328a50eb0ee26423acdf6
RevanToma <tamrevan@gmail.com>
2023-09-18 16:59:44 +0200

    optimized lazy loading

--- src/route.js
+++ src/route.js
@@ -1,14 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import ContactCoach from './pages/requests/ContactCoach.vue';
-import RequestsReceived from './pages/requests/RequestsReceived.vue';
+// import RequestsReceived from './pages/requests/RequestsReceived.vue';
 
-import CoachDetails from './pages/coaches/CoachDetails.vue';
-import CoachesList from './pages/coaches/CoachesList.vue';
-import CoachRegistration from './pages/coaches/CoachRegistration.vue';
-import UserAuth from './pages/auth/UserAuth.vue';
-import NotFound from './pages/NotFound.vue';
+// import ContactCoach from './pages/requests/ContactCoach.vue';
+// import CoachDetails from './pages/coaches/CoachDetails.vue';
+// import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+// import CoachesList from './pages/coaches/CoachesList.vue';
+// import UserAuth from './pages/auth/UserAuth.vue';
+// import NotFound from './pages/NotFound.vue';
 import store from './store/index.js';
+
+const CoachDetails = () => import('./pages/coaches/CoachDetails.vue');
+const CoachRegistration = () => import('./pages/coaches/CoachRegistration.vue');
+
+const CoachesList = () => import('./pages/coaches/CoachesList.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
+const NotFound = () => import('./pages/NotFound.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
 
 const router = createRouter({
   history: createWebHistory(),
